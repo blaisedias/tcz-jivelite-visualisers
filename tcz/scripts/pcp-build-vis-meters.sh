68823fb2aab56f02dd938264b1da95133f27f552
Blaise Dias <diasblaise@gmail.com>
2025-03-26 09:59:58 +0000

    chore: add platform check for pcp script
    The script should only run on piCorePlayer
    
    Signed-off-by: Blaise Dias <diasblaise@gmail.com>

--- tcz/scripts/pcp-build-vis-meters.sh
+++ tcz/scripts/pcp-build-vis-meters.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 
 SCRIPTDIR=$(dirname $(realpath $0))
 
-tce-load -w bash
-tce-load -w git
-tce-load -w squashfs-tools
+if [ -f "/usr/local/etc/pcp/pcpversion.cfg" ] ; 
+then
 
-tce-load -il bash
-tce-load -il git
-tce-load -il squashfs-tools
+    tce-load -w bash
+    tce-load -w git
+    tce-load -w squashfs-tools
 
-$SCRIPTDIR/build-vis-meter-suites.sh
+    tce-load -il bash
+    tce-load -il git
+    tce-load -il squashfs-tools
+
+    $SCRIPTDIR/build-vis-meter-suites.sh
+else
+    echo "This script must be run on piCorePlayer"
+fi
